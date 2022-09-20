59a956ec316563a32d03381bc9285c5b459f4321
daehwan501.kim <daehwan501.kim@samsung.com>
2022-09-20 19:21:58 +0900

    high reward for eating

--- snake_machine_learning/ml/ml_config.py
+++ snake_machine_learning/ml/ml_config.py
@@ -1,5 +1,5 @@
 ml_config = {
-    "rewards": {"negative": -10, "positive": 10},
+    "rewards": {"negative": -10, "positive": 20},
     "gamma": 0.9,
     "learning_rate": 0.0005,
     "epsilon": 0,
